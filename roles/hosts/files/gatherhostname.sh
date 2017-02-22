c70491301927cbe63213448aaafb4c327b9ad7a8
Freddy Paxton <Frederick.Paxton@homeoffice.gsi.gov.uk>
2017-02-22 09:50:18 +0000

    Updated hosts task and gatherhostname script

--- roles/hosts/files/gatherhostname.sh
+++ roles/hosts/files/gatherhostname.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+NUMOFLINES=$(awk 'END {print NR}' /home/dadmin/GIT/ansible-generate-hosts/nmapips.txt)
+COUNTER=1
+
+while [ $COUNTER != $NUMOFLINES ]; do
+	IP=$(sed -n "${COUNTER}p" /home/dadmin/GIT/ansible-generate-hosts/nmapips.txt)
+	scp dadmin@${IP}:/etc/hostname /home/dadmin/GIT/ansible-generate-hosts
+	sed -i /home/dadmin/GIT/ansible-generate-hosts/hostname -e 's/$/ ansible_host=IP ansible_port=22 ansible_user=dadmin/' 
+	sed -i "s/IP/${IP}/g" /home/dadmin/GIT/ansible-generate-hosts/hostname
+	mv /home/dadmin/GIT/ansible-generate-hosts/hostname /home/dadmin/GIT/ansible-generate-hosts/${IP}
+	let COUNTER=COUNTER+1
+done
+
+

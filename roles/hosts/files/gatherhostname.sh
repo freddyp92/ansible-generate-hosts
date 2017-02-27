ec9be70cbba86609c966e836dc8b8c6265f4acc0
Freddy Paxton <Frederick.Paxton@homeoffice.gsi.gov.uk>
2017-02-27 11:48:57 +0000

    added assemble module to merge IP files into one hosts file

--- roles/hosts/files/gatherhostname.sh
+++ roles/hosts/files/gatherhostname.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-NUMOFLINES=$(awk 'END {print NR}' /home/dadmin/GIT/ansible-generate-hosts/nmapips.txt)
+NUMOFLINES=$(awk 'END {print NR}' /home/dadmin/GIT/ansible-generate-hosts/roles/hosts/files/nmapips.txt)
 COUNTER=1
 
 while [ $COUNTER != $NUMOFLINES ]; do
-	IP=$(sed -n "${COUNTER}p" /home/dadmin/GIT/ansible-generate-hosts/nmapips.txt)
-	scp dadmin@${IP}:/etc/hostname /home/dadmin/GIT/ansible-generate-hosts
-	sed -i /home/dadmin/GIT/ansible-generate-hosts/hostname -e 's/$/ ansible_host=IP ansible_port=22 ansible_user=dadmin/' 
-	sed -i "s/IP/${IP}/g" /home/dadmin/GIT/ansible-generate-hosts/hostname
-	mv /home/dadmin/GIT/ansible-generate-hosts/hostname /home/dadmin/GIT/ansible-generate-hosts/${IP}
+	IP=$(sed -n "${COUNTER}p" /home/dadmin/GIT/ansible-generate-hosts/roles/hosts/files/nmapips.txt)
+	scp dadmin@${IP}:/etc/hostname /home/dadmin/GIT/ansible-generate-hosts/roles/hosts/files/hosts
+	sed -i /home/dadmin/GIT/ansible-generate-hosts/roles/hosts/files/hosts/hostname -e 's/$/ ansible_host=IP ansible_port=22 ansible_user=dadmin/' 
+	sed -i "s/IP/${IP}/g" /home/dadmin/GIT/ansible-generate-hosts/roles/hosts/files/hosts/hostname
+	mv /home/dadmin/GIT/ansible-generate-hosts/roles/hosts/files/hosts/hostname /home/dadmin/GIT/ansible-generate-hosts/roles/hosts/files/hosts/${IP}
 	let COUNTER=COUNTER+1
 done
 
